0c3c11d17681c79ece28c992c1cd3cb3f7d09296
vladimirskaya <63165081+vladimirskaya@users.noreply.github.com>
2021-03-01 23:33:44 +0300

    feat: addEventListener for buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 'use strict';
 
 class Todo {
-    constructor(form, input, todoList, todoCompleted) {
+    constructor(form, input, todoList, todoCompleted, todoContainer) {
         this.form = document.querySelector(form);
         this.input = document.querySelector(input);
         this.todoList = document.querySelector(todoList);
         this.todoCompleted = document.querySelector(todoCompleted);
+		this.todoContainer = document.querySelector(todoContainer);
         this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
     }
 
     addToStorage(){
-        console.log(this.todoData);
-        console.log([...this.todoData]);
         localStorage.setItem('toDoList',JSON.stringify([...this.todoData]));
     }
 
@@ -26,7 +25,8 @@ class Todo {
         const li = document.createElement('li');
         li.classList.add('todo-item');
         li.key = todo.key;
-        console.log(todo.value);
+		/*console.log('li.key = ', li.key);
+        console.log(todo.value);*/
         li.insertAdjacentHTML('beforeend', `
             <span class="text-todo">${todo.value}</span>
             <div class="todo-buttons">
@@ -44,6 +44,7 @@ class Todo {
 
     addTodo(e) {
         e.preventDefault(); 
+		//сюда передается значение this - форма, но метод забиндили с объектом 
         if (this.input.value.trim()) {
             const newTodo = {
                 value: this.input.value,
@@ -53,32 +54,60 @@ class Todo {
             this.todoData.set(newTodo.key, newTodo);
             this.render();
         }
-        console.log(this);
-
     }
+	
     generateKey() {
         return Math.random().toString(36).substring(2, 15);
     }
 
-    deleteItem(){
+    deleteItem(_this, liDelKey){
         /*по ключу найти элемент и удалить его из Мар
         после этого сделать рендер*/ 
+		_this.todoData.delete(liDelKey);
+		_this.render();
+		//this.handle();
+		//найти в коллекции 
     }
-    completedItem(){
-        /**перебрать все желменты и найти тот элемент, 
-         * на который мы нажали и поменять его свойство комплитед /
-// handler - на какую их кнопок вы нажали, используя делегирование
-// и вызвать один из следующих методов: deleteItem / completedItem
-
-самое сложно это найти key
-в видео добавлют ключ на страницу: li.key = todo.key? строка 28*/
-
+    completedItem(_this, liToggleKey){
+		let item = _this.todoData.get(liToggleKey);
+		//console.log(_this.todoData.get(liToggleKey));
+		for (let i in item){
+			if (i === 'completed') {
+				//console.log(item[i])
+				item[i] = !item[i];
+				//console.log(item[i])
+			}	
+		}
+		_this.render();	
+	}
+	
+	handle(e){
+		//для того чтобы осуществить делегирование, найдем общего родителя. как варик есть два списка, а есть общий контейнер.
+		let btnTodoRemove = document.querySelectorAll('.todo-remove'),
+			btnTodoComplete = document.querySelectorAll('.todo-complete'),
+			target = event.target,
+			btnTarget = target.closest('button'), // найдет ближайшую кнопку
+			liTarget = target.closest('li');  // ближайшая li
+			//console.log(btnTarget,liTarget);
+			
+			btnTodoRemove.forEach((value) =>{
+				if (btnTarget === value){
+					this.deleteItem(this, liTarget.key);
+				}
+			});
+			btnTodoComplete.forEach((value) =>{
+				if (btnTarget === value){
+					this.completedItem(this, liTarget.key);
+				}
+			});
+	}
 
     init() {
         this.form.addEventListener('submit', this.addTodo.bind(this));
+		this.todoContainer.addEventListener('click',this.handle.bind(this));
         this.render();
     }
 }
-const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
+const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed','.todo-container');
 
 todo.init();
